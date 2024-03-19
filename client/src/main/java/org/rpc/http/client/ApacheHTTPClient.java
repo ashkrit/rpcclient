bef567ad2682f4f70812e3a1afbe80e485fbe963
ashkrit@gmail.com <ashkrit@gmail.com>
2024-03-19 21:24:06 +0800

    Add logger

--- client/src/main/java/org/rpc/http/client/ApacheHTTPClient.java
+++ client/src/main/java/org/rpc/http/client/ApacheHTTPClient.java
@@ -8,22 +8,29 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class ApacheHTTPClient implements XHttpClient {
+
+    private final Logger logger = LoggerFactory.getLogger(ApacheHTTPClient.class);
+
+    public static final int CODE_OK = 200;
+
     @Override
     public void get(String url, Map<String, String> headers, XHttpClientCallback callback) {
 
+        logger.info("Calling {}", url);
         HttpGet method = new HttpGet(url);
         headers.forEach(method::addHeader);
-
 
         try (CloseableHttpClient httpclient = HttpClients.createDefault();
              CloseableHttpResponse response = httpclient.execute(method);) {
             int code = response.getCode();
             String reply = EntityUtils.toString(response.getEntity());
-            if (code == 200) {
+            if (code == CODE_OK) {
                 callback.onComplete(XHttpResponse.success(reply));
             } else {
                 callback.onComplete(XHttpResponse.error(code, reply));
@@ -45,7 +52,7 @@ public class ApacheHTTPClient implements XHttpClient {
              CloseableHttpResponse response = httpclient.execute(postMethod);) {
             int code = response.getCode();
             String reply = EntityUtils.toString(response.getEntity());
-            if (code == 200) {
+            if (code == CODE_OK) {
                 callback.onComplete(XHttpResponse.success(reply));
             } else {
                 callback.onComplete(XHttpResponse.error(code, reply));
