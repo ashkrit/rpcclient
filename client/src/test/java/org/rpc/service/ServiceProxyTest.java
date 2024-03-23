f90e07abf312d9da364b5947bab354f8c193e676
ashkrit@gmail.com <ashkrit@gmail.com>
2024-03-23 13:13:10 +0800

    Get verified

--- client/src/test/java/org/rpc/service/ServiceProxyTest.java
+++ client/src/test/java/org/rpc/service/ServiceProxyTest.java
@@ -3,19 +3,24 @@ package org.rpc.service;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.rpc.http.XBody;
+import org.rpc.http.XGET;
+import org.rpc.http.XHeaders;
+import org.rpc.http.XPOST;
 import org.rpc.http.client.XHttpClient;
-import org.rpc.service.main.EmbeddingService;
+import org.rpc.service.main.model.Embedding;
 import org.rpc.service.main.model.ModelInfo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ServiceProxyTest {
 
     @Test
-    public void test_get_method() {
+    public void test_get_with_no_params() {
 
         ModelInfo modelInfo = new ModelInfo();
         modelInfo.models = new ArrayList<>();
@@ -23,12 +28,16 @@ public class ServiceProxyTest {
         modelInfo.models.add(new ModelInfo.Model("bert-base-uncased", "local"));
         modelInfo.models.add(new ModelInfo.Model("bert-base-cased", "local"));
 
+        Map<String, String> callParams = new HashMap<>();
 
         RpcBuilder builder = new RpcBuilder()
                 .serviceUrl("http://nohost.com/")
                 .client(new XHttpClient() {
                     @Override
                     public void get(String url, Map<String, String> headers, XHttpClientCallback callback) {
+                        callParams.putAll(headers);
+                        callParams.put("url", url);
+
                         callback.onComplete(XHttpResponse.success(new Gson().toJson(modelInfo)));
                     }
 
@@ -38,16 +47,30 @@ public class ServiceProxyTest {
                     }
                 });
 
-        EmbeddingService service = builder.create(EmbeddingService.class);
+        SuperService service = builder.create(SuperService.class);
 
         RpcReply<ModelInfo> reply = service.list();
 
         reply.execute();
 
-        Assertions.assertAll(
-                () -> assertTrue(reply.isSuccess())
+        assertAll(
+                () -> assertEquals("http://nohost.com/list", callParams.get("url")),
+                () -> assertEquals("application/json", callParams.get("Content-Type")),
+                () -> assertTrue(reply.isSuccess()),
+                () -> Assertions.assertEquals(modelInfo.models.get(0).name, reply.value().models.get(0).name)
         );
 
 
+    }
+
+    public interface SuperService {
+        @XGET("/list")
+        @XHeaders({"Content-Type: application/json"})
+        RpcReply<ModelInfo> list();
+
+
+        @XPOST("/embedding")
+        @XHeaders({"Content-Type: application/json"})
+        RpcReply<Embedding.EmbeddingReply> embedding(@XBody Embedding embedding);
     }
 }
