cd6880c71228a83d505dc95ee954dc2394c610e0
ashkrit@gmail.com <ashkrit@gmail.com>
2024-03-23 17:41:13 +0800

    Use DDG raw api

--- client/src/main/java/org/rpc/service/RpcReply.java
+++ client/src/main/java/org/rpc/service/RpcReply.java
@@ -5,17 +5,10 @@ import java.util.Optional;
 public interface RpcReply<T> {
 
     T value();
-
     void execute();
-
     int statusCode();
-
     boolean isSuccess();
-
     Optional<String> reply();
-
     Optional<String> error();
-
     Optional<Exception> exception();
-
 }
